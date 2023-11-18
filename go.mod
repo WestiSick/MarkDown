f8be626559936a694127b1374493e5d499fffa18
westysick <vide45@yandex.ru>
2023-11-18 23:10:15 +0300

    add markup

--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module MarkDown
 go 1.21
 
 require github.com/joho/godotenv v1.5.1
+require github.com/russross/blackfriday v1.6.0 // indirect
--- main.go
+++ main.go
@@ -4,18 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
-
-type GitHubTreeResponse struct {
-	Tree []struct {
-		Path string `json:"path"`
-		Type string `json:"type"`
-	} `json:"tree"`
-}
 
 func init() {
 	// загружаем значения из файла .env
@@ -26,6 +20,7 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", fileListHandler)
+	http.HandleFunc("/file", fileHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -34,55 +29,70 @@ func fileListHandler(w http.ResponseWriter, r *http.Request) {
 	repo := os.Getenv("REPO")
 	branch := os.Getenv("BRANCH")
 
-	token := os.Getenv("TOKEN")
-
 	fileURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", owner, repo, branch)
-	req, err := http.NewRequest("GET", fileURL, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	req.Header.Set("Authorization", "token "+token)
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.Get(fileURL)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		http.Error(w, "Запрос к API GitHub не удался: "+resp.Status, resp.StatusCode)
-		return
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-
-	var tree GitHubTreeResponse
-	err = json.Unmarshal(body, &tree)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var markdownFiles []string
-	for _, file := range tree.Tree {
-		if file.Type == "blob" && len(file.Path) >= 3 && file.Path[len(file.Path)-3:] == ".md" {
-			markdownFiles = append(markdownFiles, file.Path)
-		}
 	}
 
 	fmt.Fprintln(w, "<html><body>")
-	for _, file := range markdownFiles {
-		link := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s", owner, repo, branch, file)
-		fmt.Fprintf(w, "<a href=\"%s\" target=\"_blank\">%s</a><br>", link, file)
+	if tree, ok := result["tree"].([]interface{}); ok {
+		for _, file := range tree {
+			if fileInfo, ok := file.(map[string]interface{}); ok {
+				path, pathOk := fileInfo["path"].(string)
+				fileType, typeOk := fileInfo["type"].(string)
+				if pathOk && typeOk && fileType == "blob" && len(path) > 3 && path[len(path)-3:] == ".md" {
+					fmt.Fprintf(w, "<a href=\"/file?owner=%s&repo=%s&branch=%s&path=%s\">%s</a><br>", owner, repo, branch, path, path)
+				}
+			}
+		}
 	}
 	fmt.Fprintln(w, "</body></html>")
+}
 
-	log.Println("Запрос успешно обработан")
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	owner := r.URL.Query().Get("owner")
+	repo := r.URL.Query().Get("repo")
+	branch := r.URL.Query().Get("branch")
+	path := r.URL.Query().Get("path")
+
+	fileContent, err := getFileContent(owner, repo, branch, path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	htmlContent := blackfriday.MarkdownCommon([]byte(fileContent))
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write(htmlContent)
+}
+
+func getFileContent(owner, repo, branch, path string) (string, error) {
+	fileURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s", owner, repo, branch, path)
+
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("запрос к %s не удался: %s", fileURL, resp.Status)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
